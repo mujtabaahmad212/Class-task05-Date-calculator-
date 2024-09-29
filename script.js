067199688322435102eda1b690336c763121b7d8
Mujtaba Ahmad <140813692+mujtabaahmad212@users.noreply.github.com>
2024-09-29 12:36:13 +0500

    geting new date using js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+function culcolateTheDate() {
+    const startDateInput = document.getElementById('startDate');
+    const StartDate = new Date(startDateInput.value)
+    const currentDate = new Date ();
 
+    const years = currentDate.getFullYear() - StartDate.getFullYear();
+    const months = currentDate.getMonth( ) - StartDate.getMonth();
+    const days = currentDate.getDay()  -StartDate.getDay()
+     
+    
+}
