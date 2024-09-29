cbf5ef72ae220877783356581d73ae2829095764
Mujtaba Ahmad <140813692+mujtabaahmad212@users.noreply.github.com>
2024-09-29 13:01:24 +0500

    cculcolating date

--- script.js
+++ script.js
@@ -5,7 +5,23 @@ function culcolateTheDate() {
 
     const years = currentDate.getFullYear() - StartDate.getFullYear();
     const months = currentDate.getMonth( ) - StartDate.getMonth();
-    const days = currentDate.getDay()  -StartDate.getDay()
-     
+    const days = currentDate.getDay()  -StartDate.getDay();
+
+    if ( days < 0){
+        months--;
+        const daysInlastmonth = new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, 0).getDate();
+        days += daysInlastmonth;
+        
+        
     
+}
+if (months <0){+-
+
+    years--;
+    months += 12;
+
+}
+document.getElementById('years').textContent = years;
+document.getElementById('months').textContent = months;
+document.getElementById('days').textContent = days;
 }
